e53d0128625d5c0a2318ed4578d233aaaf1704bb
kananichirag <chiragkanani144@gmail.com>
2025-09-27 02:19:36 +0530

    update and get users

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,3 +50,43 @@ module.exports.Login = async (req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 }
+
+module.exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({ users });
+    } catch (error) {
+        console.log('✌️error --->', error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+module.exports.UpdateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json({ message: "User ID is required" });
+        }
+
+        const { name, email, blood_group, location, age, gender, type } = req.body;
+        const updatedUser = await User.findByIdAndUpdate(id, {
+            name,
+            email,
+            blood_group,
+            location,
+            age,
+            gender,
+            type
+        }, { new: true });
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({ message: "User updated successfully", user: updatedUser });
+    } catch (error) {
+        console.log('✌️error --->', error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
